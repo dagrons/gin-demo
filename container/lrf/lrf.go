7c4b2a7ebfce7e551f8fb73f6b2d2e26a7a205f5
dagrons <heyuehuii@126.com>
2022-03-05 18:01:46 +0800

    update

--- container/lrf/lrf.go
+++ container/lrf/lrf.go
@@ -1,10 +1,8 @@
 package lrf
 
-import (
-	"container/list"
-)
+import "container/list"
 
-type Pair struct {
+type PIII struct {
 	K, V, F int
 }
 
@@ -12,65 +10,54 @@ type LFUCache struct {
 	Map     map[int]*list.Element
 	ListMap map[int]*list.List
 	Cap     int
-	Min     int // min frequency
+	Min     int
+}
+
+func Constructor(cap int) LFUCache {
+	return LFUCache{map[int]*list.Element{}, map[int]*list.List{}, cap, 1}
 }
 
 func (l *LFUCache) Get(k int) int {
-	if e, ok := l.Map[k]; ok {
-		v := e.Value.(Pair)
-		l.ListMap[v.F].Remove(e)
-		if l.ListMap[v.F].Len() == 0 {
-			delete(l.ListMap, v.F)
-			if l.Min == v.F {
-				l.Min++
-			}
-		}
-		v.F++
-		el, ok := l.ListMap[v.F]
-		if !ok {
-			l.ListMap[v.F] = list.New()
-		}
-		el.PushFront(v)
-		return v.V
+	e, ok := l.Map[k]
+	if !ok { // 不存在，直接返回-1
+		return -1
 	}
-	return -1
+	// 存在，上升到上一级列表
+	v := e.Value.(*PIII)
+	el := l.ListMap[v.F]
+	el.Remove(e)
+	if el.Len() == 0 && l.Min == v.F {
+		l.Min++
+	}
+	v.F++
+	if _, ok := l.ListMap[v.F]; !ok {
+		l.ListMap[v.F] = list.New()
+	}
+	l.ListMap[v.F].PushBack(v)
+	return v.V
 }
 
 func (l *LFUCache) Put(key, val int) {
-	if e, ok := l.Map[key]; ok {
-		v := e.Value.(Pair)
-		l.ListMap[v.F].Remove(e)
-		if l.ListMap[v.F].Len() == 0 {
-			delete(l.ListMap, v.F)
-			if l.Min == v.F {
-				l.Min++
-			}
-		}
+	if l.Cap == 0 {
+		return
+	}
+	if e, ok := l.Map[key]; ok { // 已经存在，更新即可
+		v := e.Value.(*PIII)
 		v.V = val
-		v.F++
-		el, ok := l.ListMap[v.F]
-		if !ok {
-			l.ListMap[v.F] = list.New()
-		}
-		el.PushFront(v)
-	} else {
-		el, ok := l.ListMap[1]
-		if !ok {
-			l.ListMap[1] = list.New()
-		}
-		v := Pair{key, val, 1}
-		e := el.PushBack(v)
-		l.Map[key] = e
-		l.Min = 1 // 新增元素频率为1
+		l.Get(key)
+		return
+	} // 还不存在，首先判断容量，再创建并加入
+	if len(l.Map) == l.Cap {
+		e := l.ListMap[l.Min].Front()
+		l.ListMap[l.Min].Remove(e)
+		delete(l.Map, e.Value.(*PIII).K)
 	}
-	if len(l.Map) > l.Cap { // 一定是新增元素造成的，所以Min一定为1
-		el := l.ListMap[l.Min]
-		e := el.Front()
-		delete(l.Map, e.Value.(Pair).K)
-		el.Remove(e)
-		if el.Len() == 0 {
-			delete(l.ListMap, l.Min)
-		}
-		l.Min = 1
+	l.Min = 1
+	nv := &PIII{key, val, 1}
+	if _, ok := l.ListMap[1]; !ok {
+		l.ListMap[1] = list.New()
 	}
+	l.Map[key] = l.ListMap[1].PushBack(nv)
 }
+
+// 什么时候会加key，什么时候会减key
